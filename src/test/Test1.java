675a6fea73ecbad9323fa8d6e356ebe3e161ee21
Mahananda Sarangmath <msarangmath8@gmail.com>
2023-11-21 18:50:44 +0530

    first commit

---
+++ src/test/Test1.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Test1 {
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes") {
+			System.out.println("eat somthing");
+		}else {
+				System.out.println("do work");
+			}
+		}
+		
+	}
+	
+
+
